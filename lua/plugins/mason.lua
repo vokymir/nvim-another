d06a4a21e3f14bbbf50e1420612b7b2b45bafc14
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-02-15 20:51:43 +0100

    after following tutorial

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end
+            })
+        end
+    },
+}
