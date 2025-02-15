bca4b92666cb9094e737532e412f58e5e8c70b72
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-02-15 21:58:39 +0100

    add telescope, oil and mini plugins

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,12 @@
+return { {
+    'echasnovski/mini.nvim',
+    enabled = true,
+    config = function()
+        local statusline = require 'mini.statusline'
+        statusline.setup { use_icons = true }
+
+        require('mini.pairs').setup {}
+        require("mini.git").setup({})
+        require("mini.indentscope").setup({})
+    end
+}, }
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+    {
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+            "sharkdp/fd"
+        },
+
+        config = function()
+            require('telescope').setup {
+                extensions = {
+                    fzf = {}
+                }
+            }
+
+            require('telescope').load_extension('fzf')
+
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+            vim.keymap.set("n", "<leader>fn", function()
+                builtin.find_files {
+                    cwd = vim.fn.stdpath("config")
+                }
+            end)
+        end
+    }
+}
